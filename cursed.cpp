2e8c6f55485d3f1a6c0248c9a5b79c6147f4ebf5
kernaltrap <94473358+JamesIsWack@users.noreply.github.com>
2023-12-05 12:14:30 -0600

    Create cursed.cpp

---
+++ cursed.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Cursed
+    if (18,446,744,073,709,551,615 == -1) {
+       return true;
+}   else {
+       // I wish this statement passed
+       return false;
+}
+    return 0;
+}
